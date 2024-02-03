a82c6ea809fb2de97c51326704088add70d4d025
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-02-03 12:51:24 -0300

    feat: ensure search component is showed on search page

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -4,6 +4,7 @@ import BarbershopItem from "../(home)/_components/barbershop-item";
 import Header from "../_components/header";
 import { db } from "../_lib/prisma";
 import { redirect } from "next/navigation";
+import Search from "../(home)/_components/search";
 
 interface BarbershopsPageProps {
   searchParams: {
@@ -26,12 +27,13 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
   return (
     <>
       <Header />
-      <div className="px-5 py-6">
+      <div className="px-5 py-6 flex flex-col gap-6">
+        <Search />
         <h1 className="text-gray-400 font-bold text-xs uppercase">
           Resultados para "{searchParams.search}"
         </h1>
 
-        <div className="grid grid-cols-2 gap-4 mt-3">
+        <div className="grid grid-cols-2 gap-4">
           {barbershops.map((barbershop: { id: Key | null | undefined }) => (
             <div key={barbershop.id} className="w-full">
               <BarbershopItem barbershop={barbershop} />
