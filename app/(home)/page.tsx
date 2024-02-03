3f0ad304bdedde7803afe441f928cff1340555b9
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-02-03 11:35:48 -0300

    fix: fixing deploy

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,8 +8,6 @@ import BarbershopItem from "./_components/barbershop-item";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../_lib/auth";
 import { Key } from "react";
-import { Key } from "react";
-import { Key } from "react";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
