96bf1e65c392527b04386595ea344a9b177003f0
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-02-26 09:06:27 -0300

    fix: fixing deploy

--- next.config.js
+++ next.config.js
@@ -7,13 +7,4 @@ const nextConfig = {
     },
 };
 
-const path = require('path');
-
-module.exports = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
-};
-
 export default nextConfig;
